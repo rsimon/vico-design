b2198e42f0e02cbfce4f3ff947135c1b8f80b384
Rainer Simon <rainer@rainersimon.io>
2023-03-02 10:12:04 +0100

    Basic structure for OSD annotation page

---
+++ src/components/annotation/ImageAnnotationDesktop/ImageAnnotationDesktop.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useRef } from 'react';
+import OpenSeadragon from 'openseadragon';
+
+import './ImageAnnotationDesktop.css';
+
+interface ImageAnnotationDesktopProps {
+
+  // for future use
+
+}
+
+const DUMMY_IMAGE = {
+  "@context" : "http://iiif.io/api/image/2/context.json",
+  "protocol" : "http://iiif.io/api/image",
+  "width" : 7808,
+  "height" : 5941,
+  "sizes" : [
+     { "width" : 244, "height" : 185 },
+     { "width" : 488, "height" : 371 },
+     { "width" : 976, "height" : 742 }
+  ],
+  "tiles" : [
+     { "width" : 256, "height" : 256, "scaleFactors" : [ 1, 2, 4, 8, 16, 32 ] }
+  ],
+  "@id" : "https://iiif.bodleian.ox.ac.uk/iiif/image/af315e66-6a85-445b-9e26-012f729fc49c",
+  "profile" : [
+     "http://iiif.io/api/image/2/level2.json",
+     { "formats" : [ "jpg", "png", "webp" ],
+       "qualities" : ["native","color","gray","bitonal"],
+       "supports" : ["regionByPct","regionSquare","sizeByForcedWh","sizeByWh","sizeAboveFull","sizeUpscaling","rotationBy90s","mirroring"],
+       "maxWidth" : 1000,
+       "maxHeight" : 1000
+     }
+  ]
+};
+
+export const ImageAnnotationDesktop = (props: ImageAnnotationDesktopProps) => {
+
+  const osd = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (osd.current) {
+      const viewer = OpenSeadragon({
+        element: osd.current,
+        prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@3.1/build/openseadragon/images/',
+        tileSources: DUMMY_IMAGE,
+        gestureSettingsMouse: {
+          clickToZoom: false
+        }
+      }); 
+    }
+  }, [ osd.current ])
+
+  return (
+    <div className="ia-desktop-container">
+      <div ref={osd} className="ia-osd-container">
+        {/* OSD mounts here */}
+      </div>
+    </div>
+  )
+
+}
