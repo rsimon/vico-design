6069fddf9f19010efd32c819cfbd89c554e3c1ed
Rainer Simon <rainer@rainersimon.io>
2023-03-06 09:48:06 +0100

    Started outlining annotation type + sidebar list

--- src/components/annotation/ImageAnnotationDesktop/AnnotationCard/AnnotationCard.tsx
+++ src/components/annotation/ImageAnnotationDesktop/AnnotationCard/AnnotationCard.tsx
@@ -1,6 +1,15 @@
+import * as timeago from 'timeago.js';
+import { DotsThreeVertical } from 'phosphor-react';
+import { Avatar } from '@components/common';
+import type { Annotation } from 'src/types';
+
+import './AnnotationCard.css';
+
 interface AnnotationCardProps {
 
-  // Collapsed annotation cards 
+  annotation: Annotation;
+
+  // Collapsed annotation cards show only the header
   collapsed?: boolean;
 
   // Selected annotation cards have different
@@ -11,25 +20,35 @@ interface AnnotationCardProps {
 
 export const AnnotationCard = (props: AnnotationCardProps) => {
 
+  const { annotation, collapsed, selected } = props;
+
+  // TODO could there be annotations without a body? Need to guard against this!
+  const firstBody = annotation.bodies[0];
+
+  const { creator, created } = firstBody;
+
+  const comments = annotation.bodies.filter(b => b.purpose === 'commenting')
+
+  return (
+    <article className="annotation-card">
+      <header>
+        <Avatar user={creator} />
+        <h1>
+          <span className="ac-creator">{creator.fullname || creator.id}</span>
+          <span className="ac-created">{timeago.format(created)}</span>
+        </h1>
+
+        <button className="unstyled">
+         <DotsThreeVertical />
+        </button>
+      </header>
+
+      {!props.collapsed && (
+        <p>
+          {firstBody.value}
+        </p>
+      )}
+    </article>
+  )
+
 }
-const isSelected = false; // selected annotation shows reply field
-
-const isCollapsed = false; // collapsed annotation shows only header
----
-
-<article>
-  <header>
-    <!-- avatar -->
-    <!-- username -->
-    <!-- time ago -->
-    <!-- 'more' menu -->
-  </header>
-
-  <p>
-    <!-- content -->
-  </p>
-
-  <ul>
-    <!-- tags -->
-  </ul>
-</article>
--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,49 @@ export interface User {
   avatarUrl?: string;
 
 }
+
+export type Annotation = ImageAnnotation | TextAnnotation;
+
+export interface ImageAnnotation {
+
+  target: ImageAnnotationTarget;
+
+  bodies: AnnotationBody[];
+
+}
+
+export interface TextAnnotation {
+
+  target: TextAnnotationTarget;
+
+  bodies: AnnotationBody[];
+
+}
+
+export type AnnotationTarget = ImageAnnotationTarget | TextAnnotationTarget;
+
+export interface ImageAnnotationTarget {
+
+  // for future use
+
+}
+
+export interface TextAnnotationTarget {
+  
+  // for future use
+
+}
+
+export interface AnnotationBody {
+
+  creator: User;
+
+  created: Date;
+
+  purpose: Purpose;
+
+  value: any
+
+}
+
+type Purpose = 'commenting' | 'replying' | 'tagging';
